c956b9b29a8f2dec2edc32e3c072935e20caced5
marina-fdz <marina.fdz.caceres@gmail.comgit config --global core.editor code>
2024-07-17 13:57:23 +0200

    add new exercises

--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Character } from '../../interfaces/character.interface';
 
 @Component({
@@ -10,7 +10,17 @@ export class ListComponent {
 
   @Input()
   public characterList: Character[] = [{
+    id: '1',
     name: 'Trunk',
     power: 10
   }]
+
+  @Output()
+  public onDeleteId: EventEmitter<string> = new EventEmitter();
+
+
+  onDeleteCharacter(index: string):void {
+    console.log({index})
+    this.onDeleteId.emit(index);
+  }
 }
--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DbzService } from '../services/dbz.service';
 import { Character } from '../interfaces/character.interface';
 
 @Component({
@@ -8,15 +9,18 @@ import { Character } from '../interfaces/character.interface';
 
 export class MainPageComponent {
 
-  public characters: Character[] = [{
-    name: 'Krillin',
-    power: 1000
-  }, {
-    name: 'Goku',
-    power: 9500
-  }, {
-    name: 'Vegeta',
-    power: 7500
-  }];
+  constructor( private dbzService: DbzService ) {}
+
+  get characters(): Character[] {
+    return [...this.dbzService.characters];
+  }
+
+  onDeleteCharacter(id: string):void {
+    this.dbzService.deleteCharacterById(id);
+  }
+
+  onNewCharacter(character: Character):void {
+    this.dbzService.addCharacter(character);
+  }
 
 }
